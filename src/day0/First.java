0982e076352e4ac3b75affaa9f94249cc1d565c2
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-03-26 16:10:30 -0400

    updated file

--- src/day0/First.java
+++ src/day0/First.java
@@ -2,7 +2,13 @@ package day0;
 
 public class First {
 
-    //TODO: Works on this tomorrow
-    // add some cool code
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working...");
+    }
+
+    //extra line 1
+    // extra line2
+    //extra line 3
 
 }
